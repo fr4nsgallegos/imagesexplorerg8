a5388a182c5dae3a2f13ae314fd47b6ab1e15cae
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2024-05-25 11:26:54 -0500

    first view

---
+++ lib/pages/image_explorer_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ImageExplorerPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Image Explorer"),
+      ),
+    );
+  }
+}
